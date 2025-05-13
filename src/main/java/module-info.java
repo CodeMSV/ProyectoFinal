740553356464540286784f8390f2c14d593ae439
savamidev <migue.626@gmail.com>
2025-05-13 10:51:53 +0200

    Update package names in FXML and POM files to reflect new project structure

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafinalproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafinalproject to javafx.fxml;
-    exports com.example.javafinalproject;
-}
