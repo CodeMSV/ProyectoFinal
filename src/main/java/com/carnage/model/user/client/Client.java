3c1a6289b23a15c736e5e8e31e80bbaa0ee6c8a3
savamidev <migue.626@gmail.com>
2025-05-16 18:45:13 +0200

    Add Client class to represent client users with specific attributes

---
+++ src/main/java/com/carnage/model/user/client/Client.java
@@ -0,0 +1,10 @@
+package com.carnage.model.user.client;
+
+import com.carnage.model.user.User;
+
+public class Client extends User {
+
+    public Client(String userName, String userEmail, String userPassword) {
+        super(userName, userEmail, userPassword);
+    }
+}
