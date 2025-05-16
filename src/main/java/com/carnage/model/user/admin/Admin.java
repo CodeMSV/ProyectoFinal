1f9f783252997d125bcd5f88b852be3e9143b11b
savamidev <migue.626@gmail.com>
2025-05-16 18:45:04 +0200

    Add Admin class to represent admin users with specific attributes

---
+++ src/main/java/com/carnage/model/user/admin/Admin.java
@@ -0,0 +1,11 @@
+package com.carnage.model.user.admin;
+
+import com.carnage.model.user.User;
+
+public class Admin extends User {
+
+
+    public Admin(String userName, String userEmail, String userPassword) {
+        super(userName, userEmail, userPassword);
+    }
+}
