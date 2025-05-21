e67a6a76ea7a302926dd2ef800405cafe9f4dac5
savamidev <migue.626@gmail.com>
2025-05-21 18:18:30 +0200

    Refactor User class to include an ID field and override equals, hashCode, and toString methods

--- src/main/java/com/carnage/model/user/User.java
+++ src/main/java/com/carnage/model/user/User.java
@@ -1,16 +1,60 @@
 package com.carnage.model.user;
 
+import java.util.Objects;
+
 public abstract class User {
-    protected Integer userIdentifier;
-    protected static Integer UserIdentifier = 0;
-    protected  String userName;
-    protected  String userEmail;
-    protected  String userPassword;
+
+    private Integer id;
+    protected String userName;
+    protected String userEmail;
+    protected String userPassword;
+
+    public User() {}
 
     public User(String userName, String userEmail, String userPassword) {
-        this.userIdentifier = UserIdentifier++;
         this.userName = userName;
         this.userEmail = userEmail;
         this.userPassword = userPassword;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getUserEmail() {
+        return this.userEmail;
+    }
+
+    public String getUserPassword() {
+        return this.userPassword;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                '}';
     }
 }
