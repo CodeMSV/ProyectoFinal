38613942e98d84ee0c55494969f1756c7e66b329
savamidev <migue.626@gmail.com>
2025-05-21 18:14:01 +0200

    Add phone attribute and update Client constructor and methods

--- src/main/java/com/carnage/model/user/client/Client.java
+++ src/main/java/com/carnage/model/user/client/Client.java
@@ -5,10 +5,31 @@ import com.carnage.model.user.User;
 public class Client extends User {
 
     private String address;
+    private String phone;
 
+    public Client() {}
 
-    public Client(String userName, String userEmail, String userPassword, String address) {
+    public Client(String userName, String userEmail, String userPassword, String address, String phone) {
         super(userName, userEmail, userPassword);
         this.address = address;
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+    public String getPhone() {
+        return this.phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "address='" + address + '\'' +
+                ", phone=" + phone +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                '}';
     }
 }
